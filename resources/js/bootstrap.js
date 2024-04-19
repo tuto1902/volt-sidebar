38432ae5e87c23ca61d483b73658774311038c98
Arturo <jorge_arturo_rojas@hotmail.com>
2024-04-18 19:54:33 -0600

    load aplinejs plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,10 @@
+import { collapse } from '@alpinejs/collapse';
+import { Alpine, Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+Livewire.start();
+
+Alpine.plugin(collapse);
+
 import axios from 'axios';
 window.axios = axios;
 
