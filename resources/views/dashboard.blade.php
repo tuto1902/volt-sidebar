376c5257ce6e0e296afda646cd64309d516cb54d
Arturo <jorge_arturo_rojas@hotmail.com>
2024-04-18 19:54:44 -0600

    extend from admin layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-admin-layout>
